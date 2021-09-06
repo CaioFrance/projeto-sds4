db4d0d1f18f4c25b884984067eb44a326f263d9e
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-09-06 13:55:33 -0400

    bootstrap

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import '@popperjs/core'
+import 'bootstrap/dist/css/bootstrap.css'
+import './assets/css/index.css'
+
 import App from './App';
 
 ReactDOM.render(
