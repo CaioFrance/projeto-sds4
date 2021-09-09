e3391689bf7bfb2cea0c5fa5c3f2aa01203684b9
Caio Francelino <60043104+CaioFrance@users.noreply.github.com>
2021-09-08 23:53:05 -0400

    Layers

---
+++ backend/src/main/java/com/caio/dsvendas/services/SellerService.java
@@ -0,0 +1,24 @@
+package com.caio.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caio.dsvendas.dto.SellerDTO;
+import com.caio.dsvendas.entities.Seller;
+import com.caio.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(seller -> new SellerDTO(seller)).collect(Collectors.toList());
+	}
+	
+}
